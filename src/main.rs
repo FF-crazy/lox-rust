d55748c92b224ecf7ef6cfd2f91e7c0a2726ca41
FFCrazy <13453488571@163.com>
2026-04-27 17:33:57 +0800

    fix: lox interpreter entrance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env::args;
+use std::fs::File;
+use std::io::{self, Read, Write, stdin};
+
 fn main() {
-    println!("Hello, world!");
+  let args: Vec<String> = args().collect();
+  if args.len() != 2 && args.len() != 1 {
+    println!("Usage: lox [script]");
+    return;
+  }
+  if args.len() == 1 {
+    repl();
+    return;
+  }
+  let read_result = File::open(&args[1]);
+  if let Ok(mut lox_file) = read_result {
+    let mut buf = String::new();
+    lox_file.read_to_string(&mut buf).unwrap();
+    print!("{}", buf);
+  } else {
+    println!("No such File");
+  }
+}
+
+fn repl() {
+  loop {
+    print!(">  ");
+    io::stdout().flush().expect("Failed to flush stdout");
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).expect("Failed to read");
+    println!("{}", buf);
+  }
 }
